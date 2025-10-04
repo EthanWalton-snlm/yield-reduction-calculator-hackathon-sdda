5f4e151445caf441cf6e9d1f3cc7b36061500a3c
Diyali-Devraj_slm <Diyali.Devraj@sanlam.co.za>
2025-10-04 18:28:47 +0200

    JoyUI integrated on input section

--- frontend/yield-reduction-calculator-app/src/App.jsx
+++ frontend/yield-reduction-calculator-app/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Button from "@mui/joy/Button";
+import {Input, Option, Select, Box, Typography, Textarea, Button} from "@mui/joy";
 
 import "./App.css";
 
@@ -262,202 +260,281 @@ function App() {
             </tbody>
           </table>
         </div>
-        <div class="output-box">
-          <div>
-            <div class="client-details">
-              <span class="age">How old will you be on 28 February 2025?</span>
-              <div class="age-group">
-                <div>
-                  <label class="age-blocks">65</label>
-                </div>
-                <div>
-                  <label class="age-blocks">65-74</label>
-                </div>
-                <div>
-                  <label class="age-blocks">74</label>
-                </div>
-              </div>
-              <span class="age">What is your total annual taxable income?</span>
-              <div class="textfield-wrapper">
-                <div class="textfield-container">
-                  <label class="textfield">
-                    Enter your total annual taxable income
-                  </label>
-                </div>
-                <div class="info-text-container">
-                  <span>
+        <Box className="output-box">
+          <Box>
+            <Box className="client-details">
+              <Typography className="age">
+                How old will you be on 28 February 2025?
+              </Typography>
+              <Box className="age-group">
+                <Box>
+                  <Button color="neutral"
+                  onClick={function(){}}
+                  variant="soft" className="age-blocks">{'<'} 65</Button>
+                </Box>
+                <Box>
+                   <Button color="neutral"
+                  onClick={function(){}}
+                  variant="soft" className="age-blocks">65-74</Button>
+                </Box>
+                <Box>
+                  <Button color="neutral"
+                  onClick={function(){}}
+                  variant="soft" className="age-blocks">{'>'} 74</Button>
+                </Box>
+              </Box>
+               <Typography className="age">What is your total annual taxable income? </Typography>
+              <Box className="textfield-wrapper">
+                {/* <div class="textfield-container"> */}
+                  <Textarea
+                  color="neutral"
+                  disabled={false}
+                  minRows={2}
+                  placeholder="Enter your total annual taxable income"
+                  size="md"
+                  className="textfield"
+                />
+                {/* </div> */}
+                <Box className="info-text-container">
+                  <Typography>
                     Your gross income minus deductions, e.g. Retirement Annuity,
                     Medical Aid.
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="client-details">
-              <span class="age">Total Investment Value</span>
-              <div class="textfield-wrapper">
-                <div class="textfield-container space">
-                  <label class="textfield">R</label>
-                </div>
-              </div>
-              <span class="age">Gross Annual Portfolio Return (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box className="client-details">
+              <Typography className="age">Total Investment Value</Typography>
+              <Box className="textfield-wrapper">
+                <Box className="textfield-container space">
+                  <Textarea color="neutral"
+                  disabled={false}
+                  minRows={2}
+                  placeholder="R"
+                  size="md"
+                  className="textfield">
+                      </Textarea>
+                </Box>
+              </Box>
+              <Typography className="age">Gross Annual Portfolio Return (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                  <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Return From SA Interest (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              <Typography className="age">Return From SA Interest (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                  <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">
+              <Typography className="age">
                 Return From SA Local Interest (Non-REIT) (%)
-              </span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              </Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                  <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Return From SA REIT Dividends (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              <Typography className="age">Return From SA REIT Dividends (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                  <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Return From Foreign Dividends (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              <Typography className="age">Return From Foreign Dividends (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                 <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Return From Capital Growth (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              <Typography className="age">Return From Capital Growth (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                 <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Average Portfolio Turnover (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+              <Typography className="age">Average Portfolio Turnover (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                  <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
-              <span class="age">Assumed Realised Gain On Turnover (%)</span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
-            </div>
+              <Typography className="age">Assumed Realised Gain On Turnover (%)</Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                 <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
+            </Box>
 
-            <div class="client-details">
-              <span class="age">Wrapper Type</span>
-              <select
-                class="options space"
-                id="wrapperType"
-                value={selectedRA}
-                onChange={(e) => setSelectedRA(e.target.value)}
-              >
-                <option selected>Endowment</option>
-                <option value="RA">RA</option>
-                <option>TFSA</option>
-                <option>Offshore Endowment</option>
-                <option>Local or Foreign Note</option>
-              </select>
-              <span class="age">Wrapper Annual Cost (EAC %) </span>
-              <div class="textfield-wrapper space">
-                <div class="textfield-container">
-                  <input
-                    class="textfield"
-                    type="number"
-                    min="0"
-                    step="0.1"
-                    value="0"
-                  ></input>
-                </div>
-              </div>
+            <Box className="client-details">
+              <Typography className="age">Wrapper Type</Typography>
+             <Select
+              variant="soft"
+              className="options space"
+              id="wrapperType"
+              value={selectedRA}
+              onChange={(e, newValue) => setSelectedRA(newValue)}
+              placeholder="Endowment"
+            >
+            <Option value="Endowment">Endowment</Option>
+            <Option value="RA">RA</Option>
+            <Option value="TFSA">TFSA</Option>
+            <Option value="Offshore Endowment">Offshore Endowment</Option>
+            <Option value="Local or Foreign Note">Local or Foreign Note</Option>
+            </Select>
+              <Typography className="age">Wrapper Annual Cost (EAC %) </Typography>
+              <Box className="textfield-wrapper space">
+                <Box className="textfield-container">
+                 <Input
+                  className="textfield"
+                  type="number"
+                  variant="soft"
+                  defaultValue={0}
+                  slotProps={{
+                  input: {
+                  min: 0,
+                  max: 100,
+                  step: 0.1,
+                  },
+                 }}
+                />
+                </Box>
+              </Box>
 
               {selectedRA == "RA" && (
-                <div>
-                  <span class="age">Annual RA Contribution</span>
-                  <div class="textfield-wrapper">
-                    <div class="textfield-container">
-                      <label class="textfield">
-                        Enter your annual RA contribution
-                      </label>
-                    </div>
-                    <div class="info-text-container">
-                      <span>
+                <Box>
+                  <Typography className="age">Annual RA Contribution</Typography>
+                  <Box className="textfield-wrapper">
+                    <Box className="textfield-container">
+                      <Textarea color="neutral"
+                  disabled={false}
+                  minRows={2}
+                  placeholder="Enter your annual RA contribution"
+                  size="md"
+                  className="textfield">
+                      </Textarea>
+                    </Box>
+                    <Box className="info-text-container">
+                      <Typography>
                         Your gross income minus deductions, e.g. Retirement
                         Annuity, Medical Aid.
-                      </span>
-                    </div>
-                  </div>
-                </div>
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Box>
               )}
-            </div>
+            </Box>
             {/* <button class="calculate"><span><span>Calculate</span></span></button> */}
-            <Button onClick={function () {}} variant="solid">
+            <Button onClick={function () {}} variant="solid" className="calculate">
               Calculate
             </Button>
-          </div>
-        </div>
+          </Box>
+        </Box>
       </div>
     </>
   );
