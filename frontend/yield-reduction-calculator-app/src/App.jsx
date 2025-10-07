f5d94c005adef70e37c9c855707d1b17650b4da3
Diyali21 <Diyali.Devraj@sanlam.co.za>
2025-10-07 23:05:21 +0200

    fixed layout in generated pdf, content isn't cut out anymore

--- frontend/yield-reduction-calculator-app/src/App.jsx
+++ frontend/yield-reduction-calculator-app/src/App.jsx
@@ -140,7 +140,7 @@ function App() {
       filename: "yield_calculation.pdf",
       image: { type: "jpeg", quality: 0.98 },
       html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "letter", orientation: "landscape" },
+      jsPDF: { unit: "in", format: "a2", orientation: "portrait" },
     };
 
     await html2pdf().set(options).from(element).save();
